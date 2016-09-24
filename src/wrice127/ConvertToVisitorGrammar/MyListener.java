4a01318b46467d9e7de88aab937a407221ad1701
Jae Hyuk Kwak <wrice127@gmail.com>
2016-09-24 10:59:27 -0700

    Each branch now gets a branch label for visitor pattern

--- src/wrice127/ConvertToVisitorGrammar/MyListener.java
+++ src/wrice127/ConvertToVisitorGrammar/MyListener.java
@@ -7,9 +7,34 @@ import org.antlr.v4.runtime.tree.*;
 public class MyListener extends ANTLRv4ParserBaseListener {
 
 	public String result = new String();
+	private boolean bInLabeledAlt = false;
+	private String label = new String();
+	private String parserRuleSpec = new String();
 
 	@Override public void visitTerminal(TerminalNode node) {
-		result += node.getText() + "\n";
+		result += node.getText() + " ";
+		if (bInLabeledAlt) {
+			String text = node.getText();
+			label += "_";
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				label += (Character.isLetterOrDigit(c) || c == '_'
+					? c : Integer.toHexString(c));
+			}
+		}
+	}
+	@Override public void enterParserRuleSpec(ANTLRv4Parser.ParserRuleSpecContext ctx) {
+		parserRuleSpec = ctx.RULE_REF().getSymbol().getText();
+		result += "\n";
+	}
+	@Override public void enterLabeledAlt(ANTLRv4Parser.LabeledAltContext ctx) {
+		if (bInLabeledAlt) throw new RuntimeException("Already in labeledAlt ");
+		bInLabeledAlt = true;
+	}
+	@Override public void exitLabeledAlt(ANTLRv4Parser.LabeledAltContext ctx) {
+		result += "# " + parserRuleSpec + label + "\n";
+		label = "";
+		bInLabeledAlt = false;
 	}
 }
 
